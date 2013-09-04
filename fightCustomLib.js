0ab2aceddf91473011618a98e20fbccbebe87be1
Anton Mirochnichenko <soooyc@gmail.com>
2013-09-04 14:48:15 +0400

    Create fightCustomLib.js

---
+++ fightCustomLib.js
@@ -0,0 +1,36 @@
+/*
+ * @autor Anton Mirochnichenko
+ * @library FightCustomLib v.0.0.1
+ * @description This a simple unility library, he constrain usefull function's
+ * 
+ */
+
+/*
+ * @function Extend
+ * @description This funciton made a class inheritance.
+ */
+
+function fExtend(Child, Parent) {
+//Child prototype
+    var C = function() {}
+    C.prototype = Child.prototype;
+// Use 
+    Child.prototype = new Parent();
+
+    fPropertyFoundation(Child, C);
+// Set constructor and superclass
+    Child.prototype.constructor = Child;
+    Child.superclass = Parent;
+}
+
+/*
+ * @function PropertyFoundation
+ * @description Utility @Extend function
+ */
+
+function fPropertyFoundation(Child, Member) {
+    for (var prop in Member.prototype) {
+        Child.prototype[prop] = Member.prototype[prop];
+    }
+}
+
